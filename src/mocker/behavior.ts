078eb51a05b20e1d69e7c428dc41b7a7f06efa20
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-04 21:59:19 +0800

    add unit tests to mocker

--- src/mocker/behavior.ts
+++ src/mocker/behavior.ts
@@ -14,39 +14,35 @@ const SimpleBehavior: () => MockerBehavior = () => {
   return function SimpleBehaviorStart (mocker: Mocker): MockerBehaviorStop {
     log.verbose('SimpleBehavior', 'SimpleBehaviorStart(%s)', mocker)
 
-    let running = true
+    const taskList: (() => void)[] = []
+    const loop = () => taskList.forEach(task => task())
+
+    let timer = setInterval(loop, 5000)
 
     const SimpleBehaviorStop = () => {
       log.verbose('SimpleBehavior', 'SimpleBehaviorStop()')
-      running = false
+      clearInterval(timer)
     }
 
-    async function main () {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      mocker.scan('https://github.com/wechaty/wechaty-puppet-mock', ScanStatus.Waiting)
+    mocker.scan('https://github.com/wechaty/wechaty-puppet-mock', ScanStatus.Waiting)
 
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      const user = mocker.createContact()
-      mocker.login(user)
+    const user = mocker.createContact()
+    mocker.login(user)
 
-      mocker.createContacts(4)
-      mocker.createRooms(3)
+    const contactList = mocker.createContacts(3)
+    mocker.createRoom({
+      memberIdList: [
+        user.id,
+        ...contactList.map(c => c.id),
+      ],
+    })
 
-      while (true) {
-        const contact = mocker.randomContact()
-        if (contact) {
-          contact.say().to()
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 5000))
-
-        if (!running) {
-          break
-        }
+    taskList.push(() => {
+      const contact = mocker.randomContact()
+      if (contact) {
+        contact.say().to()
       }
-    }
-
-    main().catch(e => log.error('SimpleBehavior', 'SimpleBehaviorStart() main() rejection: %s', e))
+    })
 
     return SimpleBehaviorStop
   }
--- src/mocker/mocker.spec.ts
+++ src/mocker/mocker.spec.ts
@@ -1,14 +1,49 @@
 #!/usr/bin/env ts-node
 
 import test  from 'tstest'
+import sinon from 'sinon'
+
+import { PuppetMock } from '../'
 
 import { Mocker } from './mocker'
 import { SimpleBehavior } from './behavior'
+import {
+  EventScanPayload,
+  ScanStatus,
+  EventLoginPayload,
+  EventMessagePayload,
+  MessagePayload,
+  MessageType
+}                         from 'wechaty-puppet'
 
 class MockerTest extends Mocker {
 }
 
-test('Mocker restart without problem', async (t) => {
+
+function createFixture () {
+  const mocker = new Mocker()
+  const puppet = new PuppetMock({ mocker })
+
+  const [ user, mike, mary ] = mocker.createContacts(3)
+  const room = mocker.createRoom({
+    memberIdList: [
+      mike.id,
+      mary.id,
+      user.id,
+    ],
+  })
+
+  return {
+    mocker,
+    puppet,
+    room,
+    user,
+    mary,
+    mike,
+  }
+}
+
+test('Mocker restart without problem', async t => {
   const mocker = new MockerTest()
   mocker.use(SimpleBehavior())
   mocker.puppet = {
@@ -16,7 +51,7 @@ test('Mocker restart without problem', async (t) => {
   } as any
 
   try {
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 3; i++) {
       await mocker.start()
       await mocker.stop()
       t.pass('start/stop-ed at #' + i)
@@ -25,4 +60,121 @@ test('Mocker restart without problem', async (t) => {
   } catch (e) {
     t.fail(e)
   }
+})
+
+test('Mocker.scan()', async t => {
+  const {
+    mocker,
+    puppet,
+  }         = createFixture()
+
+  const QR_CODE = 'https://github.com/wechaty'
+  const EXPECTED_PAYLOAD: EventScanPayload = {
+    status: ScanStatus.Waiting,
+    qrcode: QR_CODE,
+  }
+
+  const sandbox = sinon.createSandbox()
+  const spy = sandbox.spy()
+  puppet.on('scan', spy)
+
+  await puppet.start()
+  mocker.scan(QR_CODE, ScanStatus.Waiting)
+
+  t.ok(spy.calledOnce, 'should received the scan event')
+  t.ok(spy.calledWith(EXPECTED_PAYLOAD), 'should received expected QR CODE')
+
+  await puppet.stop()
+})
+
+test('Mocker.login()', async t => {
+  const {
+    user,
+    mocker,
+    puppet,
+  }         = createFixture()
+
+  const EXPECTED_PAYLOAD: EventLoginPayload = {
+    contactId: user.id,
+  }
+
+  const sandbox = sinon.createSandbox()
+  const spy = sandbox.spy()
+  puppet.on('login', spy)
+
+  await puppet.start()
+  mocker.login(user)
+
+  t.ok(spy.calledOnce, 'should received the login event')
+  t.ok(spy.calledWith(EXPECTED_PAYLOAD), 'should received expected login payload')
+
+  await puppet.stop()
+})
+
+test('MockContact.say().to(contact)', async t => {
+  const {
+    user,
+    mary,
+    puppet,
+  }         = createFixture()
+
+  const TEXT = 'Hello, contact!'
+
+  const future = new Promise<EventMessagePayload>(r => puppet.once('message', r))
+
+  await puppet.start()
+  user.say(TEXT).to(mary)
+
+  const { messageId } = await future
+
+  const EXPECTED_PAYLOAD: MessagePayload = {
+    fromId    : user.id,
+    id        : messageId,
+    text      : TEXT,
+    timestamp : Date.now(),
+    toId      : mary.id,
+    type      : MessageType.Text,
+  }
+
+  const payload = await puppet.messagePayload(messageId)
+
+  EXPECTED_PAYLOAD.timestamp = payload.timestamp
+
+  t.deepEqual(payload, EXPECTED_PAYLOAD, 'should received the expected contact message payload')
+
+  await puppet.stop()
+})
+
+test('MockContact.say().to(room)', async t => {
+  const {
+    user,
+    room,
+    puppet,
+  }         = createFixture()
+
+  const TEXT = 'Hello, room!'
+
+  const future = new Promise<EventMessagePayload>(r => puppet.once('message', r))
+
+  await puppet.start()
+  user.say(TEXT).to(room)
+
+  const { messageId } = await future
+
+  const EXPECTED_PAYLOAD: MessagePayload = {
+    fromId    : user.id,
+    id        : messageId,
+    text      : TEXT,
+    timestamp : Date.now(),
+    roomId    : room.id,
+    type      : MessageType.Text,
+  }
+
+  const payload = await puppet.messagePayload(messageId)
+
+  EXPECTED_PAYLOAD.timestamp = payload.timestamp
+
+  t.deepEqual(payload, EXPECTED_PAYLOAD, 'should received the expected room message payload')
+
+  await puppet.stop()
 })
