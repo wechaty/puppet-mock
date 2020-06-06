ac8d174c9a2625eeadd47f83f1323cfbfcdb4375
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-06 17:16:21 +0800

    add Room.load & Contact.load test

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -9,6 +9,23 @@ import {
   Mocker,
 }                         from '../src/'
 
+async function * wechatyFixture () {
+  const mocker  = new Mocker()
+  const puppet  = new PuppetMock({ mocker })
+  const wechaty = new Wechaty({ puppet })
+
+  try {
+    await wechaty.start()
+
+    yield {
+      mocker,
+      wechaty,
+    }
+
+  } finally {
+    await wechaty.stop()
+  }
+}
 test('integration testing', async t => {
   const mocker = new Mocker()
   const puppet = new PuppetMock({ mocker })
@@ -18,13 +35,10 @@ test('integration testing', async t => {
 })
 
 test('Contact.find() mocker.createContacts()', async t => {
-  const mocker = new Mocker()
-  const puppet = new PuppetMock({ mocker })
-  const wechaty = new Wechaty({ puppet })
-
-  try {
-    await wechaty.start()
-
+  for await (const {
+    mocker,
+    wechaty,
+  } of wechatyFixture()) {
     const CONTACTS_NUM = 5
     const [ user, mike ] = mocker.createContacts(CONTACTS_NUM)
     mocker.login(user)
@@ -35,19 +49,14 @@ test('Contact.find() mocker.createContacts()', async t => {
     const contact = await wechaty.Contact.find({ name: mike.payload.name })
     t.ok(contact, 'should find a contact by name of mike')
     t.equal(contact!.id, mike.id, 'should find the contact the same id as mike')
-  } finally {
-    await wechaty.stop()
   }
 })
 
 test('Room.find() mocker.createRooms()', async t => {
-  const mocker = new Mocker()
-  const puppet = new PuppetMock({ mocker })
-  const wechaty = new Wechaty({ puppet })
-
-  try {
-    await wechaty.start()
-
+  for await (const {
+    mocker,
+    wechaty,
+  } of wechatyFixture()) {
     const user = mocker.createContact()
     mocker.login(user)
 
@@ -60,7 +69,51 @@ test('Room.find() mocker.createRooms()', async t => {
     const room = await wechaty.Room.find({ topic: starbucks.payload.topic })
     t.ok(room, 'should find a room by topic of starbucks')
     t.equal(room!.id, starbucks.id, 'should find the room the same id as starbucks')
-  } finally {
-    await wechaty.stop()
+  }
+})
+
+test('Contact.load() mocker.createContact()', async t => {
+  for await (const {
+    mocker,
+    wechaty,
+  } of wechatyFixture()) {
+
+    const user = mocker.createContact()
+    mocker.login(user)
+
+    const FILE_HELPER_ID = 'filehelper'
+
+    const filehelper = mocker.createContact({
+      id: FILE_HELPER_ID,
+    })
+
+    const contact = await wechaty.Contact.load(FILE_HELPER_ID)
+
+    t.ok(contact, 'should load contact by id')
+    t.equal(contact!.id, filehelper.id, 'should load contact with id the same as filehelper')
+
+    await contact.ready()
+    t.deepEqual((contact as any).payload, filehelper.payload, 'should match the payload between wechaty contact & mock contact')
+  }
+})
+
+test('Room.load() mocker.createRoom()', async t => {
+  for await (const {
+    mocker,
+    wechaty,
+  } of wechatyFixture()) {
+
+    const user = mocker.createContact()
+    mocker.login(user)
+
+    const starbucks = mocker.createRoom()
+
+    const room = await wechaty.Room.load(starbucks.id)
+
+    t.ok(room, 'should load room by id')
+    t.equal(room!.id, starbucks.id, 'should load room with id the same as starbucks')
+
+    await room.ready()
+    t.deepEqual((room as any).payload, starbucks.payload, 'should match the payload between wechaty room & mock room')
   }
 })
